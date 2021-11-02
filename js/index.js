b5b5cd417f86de3e2773ae5d91062a9b09d5836a
tmdwl122 <85921913+tmdwl122@users.noreply.github.com>
2021-11-03 00:58:13 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
+
+
+
+
+
 $('html, body').stop().animate({
     scrollLeft : 0
 }, 1000)
+
 
 
 $('#nav li').eq(0).addClass('on')
@@ -18,10 +24,10 @@ $('#nav li a').on('click focus', function(e){
     } else if (num===1) {
         if ( !$('.skillContainer').hasClass('on')) {
             $('.skillContainer').addClass('on')
-            count(70, '.html', 15)
-            count(60, '.css', 18)
-            count(80, '.script', 21)
-            count(60, '.jquery', 25)
+            count(80, '.html', 15)
+            count(80, '.css', 18)
+            count(60, '.script', 21)
+            count(70, '.jquery', 25)
             count(50, '.react', 29)
         }
     }
@@ -96,10 +102,10 @@ $(window).on('scroll', function(){
         $('#nav li').eq(1).siblings().removeClass('on')
         if ( !$('.skillContainer').hasClass('on') ) {
             $('.skillContainer').addClass('on')
-            count(70, '.html', 15)
-            count(60, '.css', 16)
-            count(80, '.script', 17)
-            count(60, '.jquery', 18)
+            count(80, '.html', 15)
+            count(80, '.css', 16)
+            count(60, '.script', 17)
+            count(70, '.jquery', 18)
             count(50, '.react', 19)
         }
         $('#sect3').removeClass('on')
@@ -161,7 +167,10 @@ $('.openlist li a').on('click', function(e){
     }, 800)
 })
 
-// section1
+
+
+
+// 1번째 구역
 var text = document.getElementById('text')
 var shadow = '';
 for(var i=0; i<30; i++) {
@@ -169,64 +178,22 @@ for(var i=0; i<30; i++) {
 }
 text.style.textShadow = shadow;
 
-// 세번째 section
-// $('.portfolio a').on('click', function(){
-    
-//     $('.portfolio').addClass('on')
-//     $('.close').addClass('on')
-//     var src1 = $(this).attr('data-src1') 
-    
-
-//     $('body').append(`<div class="outlayer"><img src="${src1}" alt=""></div>`)
-//     $('.outlayer').css({
-//         position: 'fixed',
-//         top: 0,
-//         left: 0,
-//         right:0,
-//         bottom:0,
-//         backgroundColor: 'rgba(0,0,0,0.9)',
-//         zIndex: 999,  
-//         transition: '0.8',
-//         transitionDelay: '0.8',
-//     })
-    
-//     .append('<button class="close"><i class="far fa-times-circle"></i></button>')
-//     $('.outlayer .close').css({
-//         border:'none',
-//         position: 'absolute',
-//         top:'180px',
-//         right: '21%',
-//         fontSize: '40px',
-//         color: '#999',
-//     })
-//     $('.outlayer img').css({ 
-//         position: 'absolute',
-//         top:'20%',
-//         left: '25%',
-//         width: "50%",
-//         verticalAlign: 'middle'
-        
-//         })
- 
-// })
-    
-// $('body').on('click','.close',function(){
-//     $('.close').removeClass('on')
-//     $('.portfolio').removeClass('on')
-// })
 
 
-// $('body').on('click','.outlayer',function(){
-//     $('.outlayer').fadeOut(500)
-//     $('.portfolio').removeClass('on')
-// })
 
-$(".portList").on('mouseover', function(){
-    $(".portfolio .portList").addClass("on");
-})
 
-$(".portList").on('mouseout', function(){
-    $(".portfolio .portList").removeClass("on");
+
+
+
+// 3번째 구역
+$('.slideInner').slick({
+    autoplay:false,
+    slidesToShow:1,
+    autoplaySpeed: 2000,
+    slidesToScroll:1,
+    arrows:true,
+    nextArrow: '<a href="javascript:;" class="arrow prev"><img src="./img/portNext.png" alt=""></a>',
+    prevArrow: '<a href="javascript:;" class="arrow next"><img src="./img/portPrev.png" alt=""></a>',
 })
 
 
@@ -240,6 +207,7 @@ $("#email").on('mouseover', function(){
 $("#email").on('mouseout', function(){
     $("#cuboid form").removeClass("ready");
 })
+
 
 
 
